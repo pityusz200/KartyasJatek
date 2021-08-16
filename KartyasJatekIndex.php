0bc8e0eb91639fd874ab8c7de8642a88b655a05a
pityusz200 <pitymakan@gmail.com>
2021-08-16 20:58:16 +0200

    2021.08.16. 20:58

--- KartyasJatekIndex.php
+++ KartyasJatekIndex.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="boot/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles/KartyasJatekIndex.css">
     <script src="scripts/KartyasJatekIndex.js"></script>
-    <script src="/lib/jquery.min.js"></script>
+    <script src="scripts/KartyasJatek.js"></script>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <title>Kártyás Játék</title>
 </head>
 <body style="background-color: #cce0ff;">
@@ -15,16 +16,41 @@
     <button class="start" onmouseover="myScriptIN()" onmouseout="myScriptOUT()" onclick="window.location.href='KartyasJatek.php'"><h1 class="cim">Test</h1></button>
     <div class="container" style= "margin-top: 80px;">
     <?php
-
+        /*
         for ($i=0; $i < 10; $i++) {
             echo '<div class="row">';
             for ($j=0; $j < 4; $j++) { 
-                echo '<div class="col-sm-3 box"><img src="kepek/test.jpg" class="img-thumbnail" alt="kep"><h1>Kati Béla</h1></div>';
+                echo '
+                <div class="col-sm-3 box">
+                    <img src="kepek/test.jpg" class="img-thumbnail" alt="kep">
+                    <h1>Kati Béla</h1>
+                </div>';
             }
             echo '</div>';
-        }
+        }*/
     ?>
-        </div>
+
+    <script>
+        $( document ).ready(function() {
+            let ajaxArray = ajax();
+            if (ajaxArray[0] == undefined) {
+                location.reload();
+            }
+            let ajaxArray = ajax();
+            console.log(ajaxArray);
+        });
+        for (let i = 0; i < ajaxArray.length; i++) {
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
+            for (let j = 0; j < array.length; j++) {
+                const kepekDiv = document.createElement("div");
+                kepekDiv.classList.add("col-sm-3");
+                kepekDiv.innerHTML = 'Hi there!';
+            }
+        }
+    </script>
+
+    </div>
 
 </body>
 </html>
